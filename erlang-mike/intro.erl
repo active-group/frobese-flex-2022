7d78e9a5348a02a47040bb4273c047898a9f1a0f
Mike Sperber <sperber@deinprogramm.de>
2022-06-27 11:59:21 +0200

    Update.

--- erlang-mike/intro.erl
+++ erlang-mike/intro.erl
@@ -98,4 +98,5 @@ minutes_since_midnight(#time{ hour = H, minute = M }) ->
 
 % Gürteltier überfahren
 -spec run_over_dillo(#dillo{}) -> #dillo{}.
-
+run_over_dillo(#dillo{ liveness = Liveness, weight = Weight}) ->
+    #dillo{ liveness = dead, weight = Weight}
