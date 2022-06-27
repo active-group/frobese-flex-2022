583abbea3d0ac0bc70690a450d488ef81060bf3a
Mike Sperber <sperber@deinprogramm.de>
2022-06-27 12:13:27 +0200

    Update.

--- erlang-mike/intro.erl
+++ erlang-mike/intro.erl
@@ -105,7 +105,7 @@ d1() -> #dillo { liveness = alive, weight = 10 }.
 %     #dillo{ liveness = dead, weight = Weight}.
 % run_over_dillo(D) -> D#dillo{ liveness = dead}.    
 run_over_dillo(D) ->
-    #dillo { liveness = Dead, D#dillo.weight }.
+    #dillo { liveness = Dead, dillo#D.weight }.
     
 
 % Gürteltier füttern
