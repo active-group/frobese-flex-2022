e98946b640d68e87c95225e445fcf54b48e424bb
rfaraji <rfaraji@frobese.com>
2022-06-27 11:13:01 +0200

    update

--- erlang/intro.erl
+++ erlang/intro.erl
@@ -1,5 +1,5 @@
 -module(intro).
--export([welcome/0, sum/2]).
+-export([welcome/0, sum/2,pets/1]).
 
 welcome() ->
     'welcome'.
@@ -11,3 +11,13 @@ sum(A,B) ->
    io:fwrite("~w~n",[Z]). 
 
 
+
+-type pets_type() :: cat | snake | dog.
+
+-spec pets(pets_type()) -> neidig | lieb.
+pets(Pets_type) ->
+    case Pets_type of
+        cat -> 'neidig';
+        snake -> 'neidig';
+        dog -> 'lieb'
+    end.
